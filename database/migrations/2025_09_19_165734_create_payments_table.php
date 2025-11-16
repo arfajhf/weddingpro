c9fd916694f7ff19514b89ba90b065ed66673c1d
arfajhf <fjrhdytlh22@gmail.com>
2025-11-17 03:49:04 +0700

    migration

--- database/migrations/2025_09_19_165734_create_payments_table.php
+++ database/migrations/2025_09_19_165734_create_payments_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->string('kode');
             $table->boolean('status')->default(false);
             $table->string('jumlah');
             $table->string('paket');

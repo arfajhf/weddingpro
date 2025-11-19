07475f6600145d5eddb9ecd48ef55a516e692831
arfajhf <fjrhdytlh22@gmail.com>
2025-11-19 07:01:38 +0700

    index dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Enums\UserRole;
+use App\Models\UserWadding;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function admin(): View
+    public function index(Request $request)
     {
+
+
+        if(Auth::user()->role === UserRole::User){
+            
+        }
+
         return view('dashboard');
     }
 
-    public function user(): View
-    {
-        return view('dashboard');
-    }
 }
